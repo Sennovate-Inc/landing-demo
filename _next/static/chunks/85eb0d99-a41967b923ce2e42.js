d77c59c1c77e341a0d8cd4adcf9133e555bdd1ae
Shobhan Biswas <61372335+shobhanbiswas11@users.noreply.github.com>
2022-07-12 10:26:20 +0530

    First

---
+++ _next/static/chunks/85eb0d99-a41967b923ce2e42.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[815],{7704:function(t,a,n){n.d(a,{GoW:function(){return r}});var h=n(6830);function r(t){return(0,h.w_)({tag:"svg",attr:{viewBox:"0 0 24 24"},child:[{tag:"g",attr:{},child:[{tag:"path",attr:{fill:"none",d:"M0 0h24v24H0z"}},{tag:"path",attr:{d:"M16 18v2H5v-2h11zm5-7v2H3v-2h18zm-2-7v2H8V4h11z"}}]}]})(t)}}}]);
